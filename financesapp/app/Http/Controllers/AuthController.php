e93cad0c942436c71d5dc6a9d6216479192d0def
sellies16 <nudlegiroskrofnica>
2026-01-12 23:39:38 +0100

    autentifikacija

--- financesapp/app/Http/Controllers/AuthController.php
+++ financesapp/app/Http/Controllers/AuthController.php
@@ -2,9 +2,92 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    
+    public function register(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name'  =>'required|string|max:255',
+            'email'  =>'required|string|email|max:255|unique:users,email',
+            'password'  =>'required|string|min:6|confirmed',
+        ]);
+
+        if ($validator->fails()){
+            return response()->json([
+                'message'   =>'Validation did not go through.',
+                'errors' =>$validator->errors(),
+            ], 422);
+        }
+
+        $data = $validator->validated();
+
+        $user = User::create([
+            'name'  =>$data['name'],
+            'lastname'  =>$data['lastname'],
+            'email'  =>$data['email'],
+            'password'  =>$data['password'],
+        ]);
+
+        $token = $user->createToken('api_token')->plainTextToken;
+
+        return response()->json([
+            'message'   =>'Successful registration!',
+            'user'  =>$user,
+            'token' =>$token,
+        ], 201);
+
+    }
+
+
+    public function login(Request $request){
+        $validator = Validator::make($request->all(), [
+        'name'  =>'required|string|max:255',
+        'email'  =>'required|string|email|max:255|unique:users,email',
+        ]);
+
+        if ($validator->fails()){
+            return response()->json([
+                'message'   =>'Validation did not go through.',
+                'errors' =>$validator->errors(),
+            ], 422);
+        }
+
+        if(Auth::attempt($validator->validated())){
+            return response()->json([
+                'message'   =>'Wrong email or password.'
+            ], 401);
+        }
+
+        $user = Auth::user();
+
+        /** @var \App\Models\User $user */
+        $token = $user->createToken('api_token')->plainTextToken;
+
+        return response()->json([
+            'message'   =>'Successful login!',
+            'user'  =>$user,
+            'token' =>$token,
+        ], 200);
+    }
+
+    public function logout(Request $request){
+        $user = $request->user();
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message'=>'You have successfully logged out.',
+        ], 200);
+    }
+
+
+    public function me(Request $request){
+        /** @var \App\Models\User $user */
+        return response()->json($request->$user());
+    }
+
+
 }
--- financesapp/routes/api.php
+++ financesapp/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\TransferController;
@@ -7,6 +8,14 @@ use App\Http\Controllers\WalletController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function (){
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me', [AuthController::class, 'me']);
+});
 
 Route::get('/wallets', [WalletController::class, 'index']);
 Route::get('/wallets/{id}', [WalletController::class, 'show']);
